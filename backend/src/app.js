c9f65aedd9ea22e557c68c4260da09229d654ab3
maulidhamisimwalimu97 <hamisimaulid064@gmail.com>
2026-02-03 15:19:32 +0300

    create and design sublist fior Admin panel

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,12 +28,52 @@ app.get('/Admin', (req, res) => {
 
 // registrations 
 app.get('/register', (req, res) => {
-  res.render('register'); // Admin.ejs
+  res.render('register'); // Admin
 });
 
 // lists of family 
 app.get('/lists_family', (req, res) => {
-  res.render('lists_family'); // Admin.ejs
+  res.render('lists_family'); // Admin
+});
+
+// lists of Payments 
+app.get('/payments', (req, res) => {
+  res.render('payments'); // Admin
+});
+
+// lists of Family Paid
+app.get('/paid', (req, res) => {
+  res.render('paid'); // Admin
+});
+
+// lists of Family Unpaid
+app.get('/unpaid', (req, res) => {
+  res.render('unpaid'); // Admin
+});
+
+// Grant Access
+app.get('/grant_access', (req, res) => {
+  res.render('grant_access'); // Admin
+});
+
+// Revoke Access
+app.get('/revoke_access', (req, res) => {
+  res.render('revoke_access'); // Admin
+});
+
+// Register Users
+app.get('/user', (req, res) => {
+  res.render('user'); // Admin
+});
+
+// Lists of users
+app.get('/lists_user', (req, res) => {
+  res.render('lists_user'); // Admin
+});
+
+// reports for admin
+app.get('/report', (req, res) => {
+  res.render('report'); // Admin
 });
 
 module.exports = app;
