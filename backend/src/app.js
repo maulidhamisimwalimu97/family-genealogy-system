3dc614cd47e37af4b56198f515a64e63872689dd
maulidhamisimwalimu97 <hamisimaulid064@gmail.com>
2026-02-03 09:29:32 +0300

    updated UI for Admin Panel

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,18 +7,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/**
- * Serve frontend static files
- */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../../frontend/views'));
+
 app.use(express.static(path.join(__dirname, '../../frontend')));
 
 /**
- * Default route → load index.html
+ * Root → redirect to Admin
  */
 app.get('/', (req, res) => {
-  res.sendFile(
-    path.join(__dirname, '../../frontend/index.html')
-  );
+  res.redirect('/Admin');
+});
+
+/**
+ * Admin Dashboard
+ */
+app.get('/Admin', (req, res) => {
+  res.render('Admin'); // Admin.ejs
+});
+
+// registrations 
+app.get('/register', (req, res) => {
+  res.render('register'); // Admin.ejs
+});
+
+// lists of family 
+app.get('/lists_family', (req, res) => {
+  res.render('lists_family'); // Admin.ejs
 });
 
 module.exports = app;
